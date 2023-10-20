cabcac3448e98910d4e7f874dd257b186f162cbf
johnmuthua <caveomuthama@gmail.com>
2023-10-20 17:03:08 +0300

    Add reversed string

--- main.h
+++ main.h
@@ -15,4 +15,5 @@ int print_custom(va_list arg);
 int print_lower_hex(unsigned long int num);
 int print_pointer(va_list arg);
 int reverse_string(va_list args);
+int string_rot13(va_list args);
 #endif
--- string_rot13.c
+++ string_rot13.c
@@ -1,1 +1,36 @@
-#include "string_rot13.h"
+ #include "main.h"
+/**
+ * string_rot13 - a fuction that returns rot13
+ * @args: arguments
+ * Return: rot13
+ */
+int string_rot13(va_list args)
+{
+	int i, j, total = 0;
+	int k = 0;
+	char *str = va_arg(args, char*);
+	char x[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char y[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		k = 0;
+		for (j = 0; x[j] && !k; j++)
+		{
+			if (str[i] == x[j])
+			{
+				_putchar(b[j]);
+				total++;
+				k = 1;
+			}
+		}
+		if (!k)
+		{
+			_putchar(str[i]);
+			total++;
+		}
+	}
+	return (total);
+}
