af0461520a205100439322d684d7db0a783ace06
johnmuthua <caveomuthama@gmail.com>
2023-10-20 10:49:19 +0300

    Add print_chars

--- main.h
+++ main.h
@@ -1,4 +1,8 @@
 #ifndef MAIN_H_
 #define MAIN_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 int _printf(const char *format, ...);
+int _putchar(char c);
 #endif
---
+++ print_chars.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * print_chars - prints characters from a given argument
+ * @args: The arguments
+ * Return: 1 for success
+ */
+int print_chars(va_list args)
+{
+	char c;
+
+	c = va_arg(args, int);
+	_putchar(c);
+	return (1);
+}
