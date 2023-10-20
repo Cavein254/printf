fd3e19e3222088d0116a5655744459e904090530
esan-codes <danielseyi21@gmail.com>
2023-10-20 12:37:02 -0700

    main file

--- main.h
+++ main.h
@@ -5,33 +5,6 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <limits.h>
-/**
- * struct placeholder - Holds special _printf values
- * @type: a character that determines the formatting method
- * @int: function pointer
- *
- * Description: a helper for the printf function
- */
-typedef struct placeholder
-{
-	char *type;
-	int (*f)();
-} match;
-int _printf(const char *format, ...);
-int _putchar(char c);
-int print_chars(va_list args);
-int _strlen(char *s);
-int _strlen_const(const char *s);
-int print_percentage(void);
-int print_strings(va_list args);
-int print_upper_hex(unsigned int num);
-int print_custom(va_list arg);
-int print_lower_hex(unsigned long int num);
-int print_pointer(va_list arg);
-int reverse_string(va_list args);
-int string_rot13(va_list args);
-int _printf(const char *identifier, ...);
-
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
