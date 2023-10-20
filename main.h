dee2229090075aec1fdc4f9c5c94ddc86cf662fa
johnmuthua <caveomuthama@gmail.com>
2023-10-20 12:12:56 +0300

    Add print_percentage to header

--- main.h
+++ main.h
@@ -6,6 +6,7 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_chars(va_list args);
-int _strlen(char *s)
-int _strlen_const(const char *s)
+int _strlen(char *s);
+int _strlen_const(const char *s);
+void print_percentage(void);
 #endif
---
+++ print_percentage.c
@@ -0,0 +1,9 @@
+#include "main.h";
+/**
+ * print_percentage - prints the percentage sign with ascii value of 37
+ * Return: This function does not return
+ */
+void print_percentage(void)
+{
+	putchar(37);
+}
